729ab8841be964db0510b7c215d6d5df19aeb333
Ilya Chyzh <ichyzh@student.unit.ua>
2020-01-29 20:01:47 +0200

    add: Categories CRUD

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,8 +19,6 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
-    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css"></script>
-
 </head>
 <body>
     <div id="app">
@@ -78,6 +76,11 @@
         <main class="py-4">
             @auth
                 <div class="container">
+                @if(session()->has('success'))
+                    <div class="alert alert-success">
+                        {{ session()->get('success') }}
+                    </div>
+                @endif
                     <div class="row">
                         <div class="col md-4">
                             <ul class="list-group">
@@ -85,7 +88,7 @@
                                     <a href="">Posts</a>
                                 </li>
                                 <li class="list-group-item">
-                                    <a href="">Categories</a>
+                                    <a href="{{ route('categories.index') }}">Categories</a>
                                 </li>
                             </ul>
                         </div>
@@ -99,5 +102,11 @@
             @endauth
         </main>
     </div>
+
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    @yield('scripts')
+
 </body>
 </html>
