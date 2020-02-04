976d9761fe52f5096247698891fb10c586953971
Ilya Chyzh <ichyzh@student.unit.ua>
2020-02-04 18:52:52 +0200

    add: Search posts

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -11,9 +11,16 @@ class WelcomeController extends Controller
 {
     public function index()
     {
+        $q = request()->query('q');
+        if($q) {
+            $posts = Post::where('title', 'LIKE', "%{$q}%")->simplePaginate(2);
+        } else {
+            $posts = Post::simplePaginate(2);
+        }
+
         return view('welcome')
             ->with('categories', Category::all())
             ->with('tags', Tag::all())
-            ->with('posts', Post::simplePaginate(2));
+            ->with('posts', $posts);
     }
 }
